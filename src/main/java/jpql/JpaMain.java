d3e99e9ffe9101d5cc479b097e82ef36b7531c2f
Mintae <alsxo99@snu.ac.kr>
2022-08-30 20:57:41 +0900

    #3 Paging

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -13,43 +13,24 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member1 = new Member();
-            member1.setAge(24);
-            member1.setUsername("member1");
-            em.persist(member1);
+            for (int i = 0; i < 100; i++) {
 
-            em.createQuery("select m from Member m", Member.class)
-                            .getResultList();
-            em.createQuery("select t from Member m join m.team t")
-                            .getResultList();
-            em.createQuery("select o.address from Order o", Address.class)
-                            .getResultList();
-            List resultList = em.createQuery("select m.username, m.age from Member m")
+                Member member = new Member();
+                member.setAge(i);
+                member.setUsername("member" + i);
+                em.persist(member);
+            }
+
+
+            List<Member> resultList = em.createQuery("select m from Member m order by m.age asc", Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(10)
                     .getResultList();
 
-            List<Object[]> resultList1 = em.createQuery("select m.username, m.age from Member m")
-                    .getResultList();
+            for (Member member : resultList) {
+                System.out.println(member);
+            }
 
-            List<MemberDTO> resultList2 = em.createQuery("select distinct new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
-                    .getResultList();
-
-            Object o = resultList.get(0);
-            Object[] result = (Object[]) o;
-
-            MemberDTO memberDTO = resultList2.get(0);
-            System.out.println(memberDTO.getUsername());
-            System.out.println(memberDTO.getAge());
-
-//            Object[] result1 = resultList1.get(0);
-//            System.out.println(result1[0]);
-//            System.out.println(result1[1]);
-
-//            System.out.println(result[0]);
-//            System.out.println(result[1]);
-//
-//            for (Object o1 : result) {
-//                System.out.println(o1);
-//            }
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
